5acc85d667ab98c2595c0c14f30af92e5aceb9be
Bobirjon Tohirov <tokhirovbob@gmail.com>
2025-01-13 14:30:14 +0500

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 type User struct {
+	FIO      string `json:"fio" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
