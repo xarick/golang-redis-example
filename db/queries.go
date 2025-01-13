ea85f6c6a048d3bde22893cfa48ebfc9112e69a1
Bobirjon Tohirov <tokhirovbob@gmail.com>
2025-01-13 14:30:18 +0500

    Update queries.go

--- db/queries.go
+++ db/queries.go
@@ -2,14 +2,14 @@ package db
 
 import "github.com/xarick/golang-redis-example/models"
 
-func RegisterUser(username, hashedPassword string) error {
-	query := "INSERT INTO users (username, password) VALUES ($1, $2)"
-	_, err := DB.Exec(query, username, hashedPassword)
+func RegisterUser(fio, username, hashedPassword string) error {
+	query := "INSERT INTO users (fio, username, password) VALUES ($1, $2, $3)"
+	_, err := DB.Exec(query, fio, username, hashedPassword)
 	return err
 }
 
 func GetUserByName(username string) (*models.User, error) {
-	query := "SELECT username, password FROM users WHERE username = $1"
+	query := "SELECT fio, username, password FROM users WHERE username = $1"
 	var user models.User
 	err := DB.Get(&user, query, username)
 	return &user, err
